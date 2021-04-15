0294e6e2ba1a50e11cde45af43068d704a8053e0
Dominic <dominic.wenzel@mailbox.org>
2021-04-15 16:55:51 +0200

    catalan problem 3 woche 2

---
+++ catalan.js
@@ -0,0 +1,41 @@
+
+// funktion der Berechung der Catalan zahl rekrusive
+// Variable n = counter n+1
+function catalan(n){
+
+    let tempCatalanNumber = 0;
+
+
+    if (n<= 1)
+    return 1;
+
+    // Aufloesung der Formel zur berechnung der Catalan Zahl 
+    for ( let i = 0; i < n ;i++){
+        tempCatalanNumber = tempCatalanNumber + catalan(i) * catalan(n -i -1);
+    }
+
+    return tempCatalanNumber;
+
+}
+
+
+
+// counter = n in formel
+let counter = 0;
+// jeweilige Catalan Nummer
+let catalanNumber = 0;
+// Bis welche Stelle soll berechnet werden
+let numberLength = 6;
+
+
+while(catalanNumber.toString().length < numberLength){
+
+catalanNumber = catalan(counter)
+console.log(catalanNumber);
+
+counter++;
+
+}
+
+console.log("Catalan Nummer mit 16 stellen");
+console.log(catalanNumber);
