44e00e042b91b7f2d3f51e486a126af6d710717a
Dominic <dominic.wenzel@mailbox.org>
2021-04-28 23:02:33 +0200

    adding final magic number

---
+++ magicQu.js
@@ -0,0 +1,11 @@
+let result = 111111111;
+
+
+
+/* Formel zur brechung der Magischen Zahl ((n * n * n) + n)/2
+ also bei 3x3 Matrix n = 3*Magische Zahl gleich 15*/
+
+
+ let magicNumber = ((3*3*3)+3)/2;
+
+ console.log(magicNumber);
